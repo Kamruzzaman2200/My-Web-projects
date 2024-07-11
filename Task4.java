8e54b32a42cdd10d7f1efa2fe36985c3f231dbaa
MD. Kamruzzaman <103466655+Kamruzzaman2200@users.noreply.github.com>
2024-07-11 17:54:01 +0600

    Create Task4.java

---
+++ Task4.java
@@ -0,0 +1,64 @@
+public class task4 {
+    private String bankName;
+    private String branch;
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public void displayBankDetails() {
+        System.out.println("Bank Name: " + bankName);
+        System.out.println("Branch: " + branch);
+    }
+
+    public static void main(String[] args) {
+        task4 myBank = new task4();
+        myBank.setBankName("SBL Bank");
+        myBank.setBranch("Mirpur rupnagar Street");
+        myBank.displayBankDetails();
+    }
+}
+public class task4 {
+    private String bankName;
+    private String branch;
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public void displayBankDetails() {
+        System.out.println("Bank Name: " + bankName);
+        System.out.println("Branch: " + branch);
+    }
+
+    public static void main(String[] args) {
+        task4 myBank = new task4();
+        myBank.setBankName("SBL Bank");
+        myBank.setBranch("Mirpur rupnagar Street");
+        myBank.displayBankDetails();
+    }
+}
